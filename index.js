1882316c2402ea77f65665a85bfa6be1fcdd4bd1
ecemonkol <ecemonkol@gmail.com>
2024-02-09 00:25:58 +0100

    feat: add id

--- index.js
+++ index.js
@@ -12,38 +12,65 @@ const shoppingListInDB = ref(database, "shoppingList")
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const listEl = document.getElementById("shopping-list")
-const itemEl = document.createElement("li")    
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
-    push(shoppingListInDB, inputValue)
+    if ( inputFieldEl.value !== '' ){
+        push(shoppingListInDB, inputValue)
+    } else {
+        alert('write sth')
+    }
 
     clearInputFieldEl();
 })
 
 onValue(shoppingListInDB, function(snapshot) {
 
-    let itemsArray = Object.values(snapshot.val());
+    let itemsArray = Object.entries(snapshot.val());
 
     clearListEl();
 
     for (let i=0; i<itemsArray.length; i++) {
-        appendItemToListEl(itemsArray[i])
+        let currentItem = itemsArray[i];
+        let currentItemID = currentItem[0];
+        let currentItemValue = currentItem[1];
+
+        appendItemToListEl(currentItem);
     }
 
-} ) 
+    console.log(itemsArray)
+
+} )
 
 function clearInputFieldEl() {
     inputFieldEl.value = ""
 }
 
-function appendItemToListEl(itemValue) {
-    if ( inputFieldEl.value !== '' ){
-        listEl.innerHTML += `<li>${itemValue}</li>`
+function appendItemToListEl(item) {
+    if (inputFieldEl.value !== '') {
+        let itemID = item[0];
+        let itemValue = item[1]
+    
+        let newEl = document.createElement('li');
+        newEl.textContent = itemValue;
+
+        
+
+        if (!newEl.style.backgroundColor) {
+            const colors = ["#FF6347", "#ff3399", "#ffbf47"];            
+            const randomColor = colors[Math.floor(Math.random() * colors.length)];
+            newEl.style.backgroundColor = randomColor; // Apply random color if not set
+        }
+
+        listEl.appendChild(newEl);
+
+        
     }
 }
+
 
 function clearListEl() {
     listEl.innerHTML = ""
 }
+
