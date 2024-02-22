96d567b7bbdca79861d0965a9e079665f87dc385
ecemonkol <ecemonkol@gmail.com>
2024-02-22 12:04:18 +0300

    fix: delete item

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, get, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://shop-list-63cfe-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -7,90 +7,67 @@ const appSettings = {
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const shoppingListInDB = ref(database, "shoppingList")
+const shoppingListRef = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
 
-addButtonEl.addEventListener("click", function() {
+addButtonEl.addEventListener("click", function () {
+
     let inputValue = inputFieldEl.value
-    
-    if ( inputFieldEl.value !== '' ){
-        push(shoppingListInDB, inputValue)
+
+    if (inputFieldEl.value !== '') {
+        const newItem = push(shoppingListRef, inputValue);
+        get(newItem.ref).then((snapshot) => {
+            addItemToList(snapshot)
+        })
     } else {
-        alert('write sth')
+        alert('Please write an item')
     }
 
     clearInputFieldEl();
 })
 
-onValue(shoppingListInDB, function(snapshot) {
-
+get(shoppingListRef).then((snapshot) => {
     if (snapshot.exists()) {
-        let itemsArray = Object.entries(snapshot.val())
-    
-        clearShoppingListEl()
-        
-        for (let i = 0; i < itemsArray.length; i++) {
-            let currentItem = itemsArray[i]
-            let currentItemID = currentItem[0]
-            let currentItemValue = currentItem[1]
-            
-            appendItemToShoppingListEl(currentItem)
-        }    
+        snapshot.forEach((item) => {
+            addItemToList(item);
+        })
     } else {
         const noItemsText = document.createElement("h2")
         shoppingListEl.appendChild(noItemsText);
         noItemsText.innerHTML = "nothing here :/"
     }
+})
 
-} )
+function addItemToList(item) {
+    const newEl = document.createElement('li');
+    newEl.textContent = item.val();
+    addRandomColor(newEl);
 
-function clearShoppingListEl() {
-    shoppingListEl.innerHTML = ""
+    newEl.addEventListener("click", function (event) {
+        event.stopPropagation();
+        // // Remove from DOM
+        newEl.remove();
+
+        // Remove from database
+        remove(item.ref);
+    });
+
+    shoppingListEl.append(newEl);
 }
 
 function clearInputFieldEl() {
     inputFieldEl.value = ""
 }
 
-function appendItemToShoppingListEl(item) {
-
-    if (inputFieldEl.value !== '') {
-        let itemID = item[0];
-        let itemValue = item[1]
-    
-        let newEl = document.createElement('li');
-        newEl.textContent = itemValue;
-
-        addRandomColor(newEl);
-
-        newEl.addEventListener("click", function(event) {
-
-            event.stopPropagation();
-            // Remove from DOM
-            newEl.remove();
-            
-            // Remove from database
-            let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`);
-            remove(exactLocationOfItemInDB);
-        });
-
-        
-        shoppingListEl.append(newEl);
-
-        
-    }
-}
-
 
 function addRandomColor(item) {
     if (!item.style.backgroundColor) {
-        const colors = ["#FF6347", "#ff3399", "#ffbf47"];            
+        const colors = ["#FF6347", "#ff3399", "#ffbf47"];
         const randomColor = colors[Math.floor(Math.random() * colors.length)];
-        item.style.backgroundColor = randomColor; 
+        item.style.backgroundColor = randomColor;
     }
 
 }
-
